6c9b56a92c06d348b492e4220087635b20ba6b0b
Rony <rony_cof@hotmail.com>
2019-04-13 23:04:02 -0300

    funlção print com saida formatada {}.format(var)

---
+++ desafio02_1.py
@@ -0,0 +1,5 @@
+#Crie um programa que receba o nome de uma pessoa e exiba uma mensagem de boas vindas
+#usando o nome informado em uma saída formatada.
+#
+nome = input("Digite seu nome:")
+print("É um prazer te conhecer {}!".format(nome))
