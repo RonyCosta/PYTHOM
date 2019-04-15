5e772adc3232bdf4c61d4a9a29c1c324be6dc3a6
Rony Costa <rony_cof@hotmail.com>
2019-04-15 14:05:49 -0300

    Soma dois números e exibe a saída formatada

---
+++ Desafio03_01.py
@@ -0,0 +1,8 @@
+#Faça um programa em Python que leia dois números exiba a soma entre eles e exiba na tela a mensagem: 
+#A soma entre o número x e o número y é igual a: z
+#Utilize saída formatada para exibir os dados:
+#
+num1 = int(input("Digite o primeiro número:"))
+num2 = int(input("Digite o segundo número:"))
+soma = num1 + num2
+print("A soma entre o nº {} e o nº {} é {}".format(num1,num2,soma))
