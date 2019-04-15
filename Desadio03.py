5a2b08f179646ac012b52a141b41f35e8f5e2901
Rony Costa <rony_cof@hotmail.com>
2019-04-15 13:58:17 -0300

    Exibir a soma entre dois números

---
+++ Desadio03.py
@@ -0,0 +1,6 @@
+#Faça um programa Python que leia dois números e faça a soma entre eles
+#
+num1 = int(input("Digite o priemrio número:"))
+num2 = int(input("Digite o segundo número:"))
+soma = num1 + num2
+print("A soma dos números é igual a ",soma)
